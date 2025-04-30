d26b94a66a381a3ee0f086b823c2a89aa802479c
alee <aleeaouini@gmail.com>
2025-05-01 00:27:08 +0200

    Make an HTTP request

---
+++ scraper3.py
@@ -0,0 +1,11 @@
+import requests
+
+def main():
+  url = "https://news.ycombinator.com/item?id=42919502"
+  response = requests.get(url)
+  print(f"Scraping: {url}")
+  print(response)
+  print(response.content)
+
+if __name__ == "__main__":
+  main()
